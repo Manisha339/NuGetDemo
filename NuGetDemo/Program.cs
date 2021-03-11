33d0b450e4fadbc2c603a337ee0fa7eed14a5fb5
mallu <mallu@HSC-5YQM6Y2>
2021-03-11 10:10:57 +0530

    Add project files.

---
+++ NuGetDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace NuGetDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string data = "{\"name\":\"Tom S.\",\"Age\":\"14\"}";
+            Console.WriteLine(data);
+            JArray array = new JArray();
+            JValue text = new JValue("Manual Text");
+            JValue date = new JValue(new DateTime(2000, 5, 23));
+            array.Add(text);
+            array.Add(date);
+            Console.WriteLine(array.ToString());
+
+            Employee employee = new Employee();
+            employee.Id = 1;
+            employee.name="mani";
+            Console.WriteLine(JsonConvert.SerializeObject(employee));
+        
+        }
+    }
+}
